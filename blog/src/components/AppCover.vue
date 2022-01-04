e5a06e9af5164b0a97496272c5a1061b1d52a336
Moussatef <otman.moussetaf@gmail.com>
2022-01-04 11:13:30 +0100

    blog update

--- blog/src/components/AppCover.vue
+++ blog/src/components/AppCover.vue
@@ -158,6 +158,51 @@
         </div>
       </template>
     </vs-dialog>
+
+    <!-- apdate blog dialog -->
+    <template>
+      <v-row justify="center">
+        <v-dialog
+          v-model="dialog"
+          fullscreen
+          hide-overlay
+          transition="dialog-bottom-transition"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" dark v-bind="attrs" v-on="on">
+              Open Dialog
+            </v-btn>
+          </template>
+          <v-card>
+            <v-toolbar dark color="primary">
+              <v-btn icon dark @click="dialog = false">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+              <v-toolbar-title>Settings</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-toolbar-items>
+                <v-btn dark text @click="dialog = false"> Save </v-btn>
+              </v-toolbar-items>
+            </v-toolbar>
+            <v-list three-line subheader>
+              <v-subheader>User Controls</v-subheader>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Content filtering</v-list-item-title>
+                  <v-list-item-subtitle
+                    >Set the content filtering level to restrict apps that can
+                    be downloaded</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+            <v-divider></v-divider>
+          </v-card>
+        </v-dialog>
+      </v-row>
+    </template>
+  </div>
+</template>
   </div>
 </template>
 
